fec9a3dc3d6788557cc169cea1fa77e809bb20d6
Macaulay <rochamacaulay@gmail.com>
2021-03-15 17:27:59 -0400

    Code fully implemented with hardware.
    Implemented gameControls() function to describe position/movement of the analog joystick and appleEaten() function to activate a buzzer once the snake has eaten an apple. Serial.read() and Serial.write() functions used to send and receive bytes to and from the code in order for the python game "snake" to function with the hardware.

--- snake_game_controls/snake_game_controls.ino
+++ snake_game_controls/snake_game_controls.ino
@@ -1,46 +1,66 @@
 const int xPin = 0; // analog pin connected to X output
 const int yPin = 1; // analog pin connected to Y output
-  
+int buzzer = 6; // buzzer connected to pin 6
+int incomingByte = 0;
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(9600); // initialize serial communication at 9600 bps
+  pinMode(buzzer, OUTPUT); 
 }
 
 void loop() {
-  int horizontal=map((analogRead(xPin)+18), 0, 1023, -5, +5); // X position
-  int vertical=map((analogRead(yPin)+18), 0, 1023, -5, +5); // Y position
+  gameControls(); // call gameControls function
 
-  // display hotizontal and vertical positions on serial monitor according to movement with analog joystick
-  Serial.print("X-axis: ");
-  Serial.print(horizontal);
-  Serial.print("\n");
-  Serial.print("Y-axis: ");
-  Serial.print(vertical);
-  Serial.print("\n\n");
-  delay(1000);
+   // read bytes if the number of bytes at serial port > 0
+   if (Serial.available() > 0) {
+     incomingByte = Serial.read();
 
-  // initial position of joystick is X=0 Y=0
+     // if score increased by 10, beep buzzer
+     if (incomingByte += int(10)) {
+      appleEaten();
+    }
+  }
+}
 
-  // if X position is less than 0, joystick moved left
-  if (horizontal < 0) {
-    Serial.println("left");
-    delay(150); // add a delay
+void gameControls() {
+  int horizontal=map((analogRead(xPin)+18), 0, 1023, -5, +5); // X position, starts at X=0
+  int vertical=map((analogRead(yPin)+18), 0, 1023, -5, +5); // Y position, starts at Y=0
+
+  // if X position is -5, move left
+  if (horizontal == -5) {
+    Serial.write("a"); // write control 'a' to serial port
+    delay(500); // add a delay
   }
 
-  // if X position is greater than 0, joystick moved right
-  else if (horizontal > 0) {
-     Serial.println("right");
-     delay(150); // add a delay
+  // if X position is 5, move right
+  else if (horizontal == 5) {
+     Serial.write("d"); // write control 'd' to serial port 
+     delay(500); // add a delay
   }
 
-  // if Y position is less than 0, joystick moved up
-  else if (vertical < 0) {
-     Serial.println("up");
-     delay(150); // add a delay
+  // if Y position is -5, move up
+  else if (vertical == -5) {
+     Serial.write("w"); // write control 'w' to serial port
+     delay(500); // add a delay
   }
 
-  // if Y position is greater than 0, joystick moved down
-  else if (vertical > 0) {
-    Serial.println("down");
-    delay(150); // add a delay
+  // if Y position is 5, move down
+  else if (vertical == 5) {
+    Serial.write("s"); // write control 's' to serial port
+    delay(500); // add a delay
   }
+}
+
+// function for turning buzzer to beep when an apple is eaten
+void appleEaten() {
+    int i;
+
+    // output a frequency
+    for (i=0; i < 1500; i++)
+    {
+      digitalWrite(buzzer,HIGH); // turn buzzer on
+      delay(1);// wait for 1 ms
+      digitalWrite(buzzer,LOW); // turn buzzer off
+      delay(1);// wait for 1 ms
+    }
 }

ff99e1863aca448172979e9d2d7605e9bb21028e
Macaulay <rochamacaulay@gmail.com>
2021-03-23 12:39:02 -0400

    Added comments, final code.

--- snake_game_controls/snake_game_controls.ino
+++ snake_game_controls/snake_game_controls.ino
@@ -4,8 +4,8 @@ const int xPin = 0; // analog pin connected to X output
 const int yPin = 1; // analog pin connected to Y output
 int buzzer = 12; // buzzer connected to pin 6
 int incomingByte = 0; // incoming bytes at serial port
-const int MPU6050_addr=0x68;
-int16_t GyroX,GyroY,GyroZ;
+const int MPU6050_addr=0x68; // MPU6050 address
+int16_t GyroX,GyroY,GyroZ; // gyro X, Y, Z variables
 
 void setup() {
   Wire.begin();
@@ -14,13 +14,13 @@ void setup() {
   Wire.write(0);
   Wire.endTransmission(true);
   Serial.begin(9600); // initialize serial communication at 9600 bps
-  pinMode(buzzer, OUTPUT);
+  pinMode(buzzer, OUTPUT); 
 }
 
 void loop() {
-  gameControls(); // call gameControls function
+  joystickControls(); // call joystickControls() function to play the game with joystick
 
-  gyroControls();
+  gyroControls(); // call gyroControls() function to play game with gyro
 
   // read bytes if the number of bytes at serial port > 0
   if (Serial.available() > 0) {
@@ -33,7 +33,7 @@ void loop() {
   }
 }
 
-void gameControls() {
+void joystickControls() {
   int horizontal = map((analogRead(xPin) + 18), 0, 1023, -5, +5); // X position, starts at X=0
   int vertical = map((analogRead(yPin) + 18), 0, 1023, -5, +5); // Y position, starts at Y=0
 
@@ -68,29 +68,29 @@ void gyroControls() {
   Wire.endTransmission(false);
   Wire.requestFrom(MPU6050_addr,14,true);
   
-  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0;
-  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0;
+  GyroX = (Wire.read() << 8 | Wire.read()) / 131.0; // X position of gyro
+  GyroY = (Wire.read() << 8 | Wire.read()) / 131.0; // Y position of gyro
+  GyroZ = (Wire.read() << 8 | Wire.read()) / 131.0; // Z position of gyro
 
-  // if X position is -5, move left
+  // if X position is greater than 45, move left
   if (GyroX > 45) {
     Serial.write("a"); // write control 'a' to serial port
     delay(30); // add a delay
   }
 
-  // if X position is 5, move right
+  // if X position is less than -45, move right
   else if (GyroX < -45) {
     Serial.write("d"); // write control 'd' to serial port
     delay(30); // add a delay
   }
 
-  // if Y position is -5, move up
+  // if Y position is less than -45, move up
   else if (GyroY < -45) {
     Serial.write("w"); // write control 'w' to serial port
     delay(30); // add a delay
   }
 
-  // if Y position is 5, move down
+  // if Y position is greater than 45, move down
   else if (GyroY > 45) {
     Serial.write("s"); // write control 's' to serial port
     delay(30); // add a delay
